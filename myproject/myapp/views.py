6cb870986d3027ef28956772fb94fa34ce6888b6
Damiroz <57715636+Damiroz@users.noreply.github.com>
2023-11-27 02:01:13 +0300

    add user_list

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,17 +1,7 @@
-from .models import CustomUser, Game, Playtime
+from django.shortcuts import render
+from .models import CustomUser, Game
 
-def user_list():
-    users = CustomUser.objects.select_related('playtime').all()
-
-    for user in users:
-        print(f"Full Name: {user.full_name},  Game: {user.Game.title if user.Game else 'None'}")
-
-    games = Game.objects.all()
-
-    for game in games:
-        print(f"Game: {game.title}, Developer: {game.developer.full_name}")
-
-    playtimes = Playtime.objects.select_related('user', 'game').all()
-
-    for playtime in playtimes:
-        print(f"{playtime.user.full_name} played {playtime.game.title} for {playtime.hours_played} hours")
+def user_list(request):
+    users = CustomUser.objects.all()
+    context = {'users': users}
+    return render(request, 'user_list.html', context)
